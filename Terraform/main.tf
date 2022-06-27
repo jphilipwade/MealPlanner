222aa0702c227905158065cd8d35c73650f1ce38
Philip Wade <jpwade@live.co.uk>
2022-06-27 07:01:46 +0100

    add basic terraform

---
+++ Terraform/main.tf
@@ -0,0 +1,25 @@
+﻿terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+## resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.product}-${var.resource_group_prefix}-${var.environment}"
+  location = var.location
+}
+
+## application insights
+resource "azurerm_application_insights" "ai" {
+  name                = "${var.product}-${var.app_insights_prefix}-${var.environment}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,35 @@
+﻿## other
+variable "product" {
+  type        = string
+  description = "Name of the product"
+  default     = "mplan"
+}
+
+variable "location" {
+  type        = string
+  description = "Region"
+  default     = "uksouth"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment"
+  default     = "dev"
+}
+
+
+## prefixes
+variable "resource_group_prefix" {
+  type    = string
+  default = "rg"
+}
+
+variable "storage_account_prefix" {
+  type    = string
+  default = "sa"
+}
+
+variable "app_insights_prefix" {
+  type    = string
+  default = "ai"
+}
